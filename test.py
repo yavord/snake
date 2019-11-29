1a92e6f992a0d719753fce232add0fee2efe5407
Yavor Dimitrov <yavor.d0@gmail.com>
2019-11-29 10:57:52 +0200

    .

---
+++ test.py
@@ -0,0 +1,27 @@
+from ipy_lib import SnakeUserInterface
+import util
+
+
+class UiHandler():
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+    
+    def processEvent(self):
+        pass
+
+
+def main():
+    ui = SnakeUserInterface(
+        util.dimensions['width'], 
+        util.dimensions['height'], 
+        util.dimensions['scale']
+        )
+
+    while True:
+        ui.get_event()
+        
+        
+
+if __name__ == "__main__":
+    main()
---
+++ util.py
@@ -0,0 +1,6 @@
+
+dimensions = {
+    'width' : 80,
+    'height' : 80,
+    'scale' : 0.5,
+}
