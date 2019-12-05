20697e0a88493685ad6bb156165b0a64fb71527b
Yavor Dimitrov <yavor.d0@gmail.com>
2019-12-05 15:33:58 -0600

    .

--- assignment3/controller/apple.py
+++ assignment3/controller/apple.py
@@ -10,3 +10,11 @@ class AppleController:
     def getNewApple(self, width, height):
         self.apple.x = self.ui.random(width)
         self.apple.y = self.ui.random(height)
+
+    def checkApplePosition(self, snake):
+        snakePieces = snake.snake
+        for snakePiece in snakePieces:
+            if self.apple.x == snakePiece.x and self.apple.y == snakePiece.y:
+                return True
+            else:
+                return False
