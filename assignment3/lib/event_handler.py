526742bc088d8a9a00a697833e6df038805447c7
Yavor Dimitrov <yavor.d0@gmail.com>
2019-12-06 22:38:22 -0600

    .

---
+++ assignment3/lib/event_handler.py
@@ -0,0 +1,23 @@
+def eventHandler(event, snakeController, appleController, ui, width, height):
+    if event.name == 'alarm':
+        ui.clear()
+        snakeController.animateSnake()
+        snakeController.placeSnake()
+        appleController.placeApple()
+        if appleController.checkApplePosition(snakeController.snake) == True:
+            print('apple/snake check')
+        if snakeController.checkSnakePosition(appleController.apple) == True:
+            snakeController.eatApple()
+            appleController.getNewApple(width, height)
+            # while appleController.checkApplePosition(snakeController.snake) == True:
+            #     print('apple/snake check')
+            #     appleController.getNewApple(width, height)
+            #     if appleController.checkApplePosition(snakeController.snake) == False:
+            #         break
+        elif snakeController.checkSnakePosition(appleController.apple) == False:
+            snakeController.gameOver()
+        snakeController.updateDirections()
+        ui.show()
+    elif event.name == 'arrow':
+        ui.wait(20)
+        snakeController.changeDirection(event.data)
