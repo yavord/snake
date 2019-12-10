aa51517a4753fda40d11af5765e3063ff16823f9
Yavor Dimitrov <yavor.d0@gmail.com>
2019-12-09 18:14:15 -0600

    .

--- assignment3/lib/event_handler.py
+++ assignment3/lib/event_handler.py
@@ -4,16 +4,15 @@ def eventHandler(event, snakeController, appleController, ui, width, height):
         snakeController.animateSnake()
         snakeController.placeSnake()
         appleController.placeApple()
-        for snakePiece in snakeController.snake.snakeList:
-            print(snakePiece)
         if snakeController.checkSnakePosition(appleController.apple) == True:
             snakeController.eatApple()
             appleController.getNewApple(width, height)
-            while appleController.checkApplePosition(snakeController.snake) == True:
-                print('apple/snake check')
-                appleController.getNewApple(width, height)
-                if appleController.checkApplePosition(snakeController.snake) == False:
-                    break
+            for snakePiece in snakeController.snake.snakeList:
+                while appleController.checkApplePosition(snakePiece) == True:
+                    print('apple/snake check')
+                    appleController.getNewApple(width, height)
+                    if appleController.checkApplePosition(snakePiece) == False:
+                        break
         elif snakeController.checkSnakePosition(appleController.apple) == False:
             snakeController.gameOver()
         snakeController.updateDirections()
